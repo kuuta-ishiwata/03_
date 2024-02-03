63fbdd1418acaa64d306ab00f55eb5697728a871
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2024-02-03 22:07:29 +0900

    PG

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 #include "GameManager.h"
-const char kWindowTitle[] = "KE2C_02_イシワタ_クウタ";
+const char kWindowTitle[] = "LE2C_02_イシワタ_クウタ";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
